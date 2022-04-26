46141eb445b15f7c582dcb888ccfa926b73f4cbe
JavierVelasquezG <javier.velasquez@inweb.cl>
2022-04-25 20:34:48 -0400

    Implement Socket.io authentication

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 import express from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
+import jwt from 'jsonwebtoken'
 
+// Models
 import GameModel from './src/models/game.js'
+import AuthModel from './src/models/auth.js'
+
+// Port
+const PORT = 3001 || process.env.PORT
 
 const app = express()
 const httpServer = http.createServer(app)
-const io = new Server(httpServer)
-
-app.get('/', (req, res) => {
-
+const io = new Server(httpServer, {
+    cors: {
+        origin: 'http://localhost:3000'
+    }
 })
 
-httpServer.listen(3000)
+// Socket.io Middlewares
+io.use(AuthModel.tokenVerifyCallbackIO).on('connection', function (socket) {
+    console.log('Hola!')
+})
+
+app.get('/', async (req, res) => {
+    let token = await AuthModel.login('Javg')
+    res.json(token)
+})
+
+httpServer.listen(PORT)
--- src/models/auth.js
+++ src/models/auth.js
@@ -1,12 +1,34 @@
 import jwt from 'jsonwebtoken'
 import { v4 as uuid } from 'uuid'
 
+// Config
 import { secretKey } from './../config/config.js'
 
 class Auth {
     static login = async (userName) => {
         let generatedID = uuid()
-        return await jwt.sign({ userName: userName, userID: generatedID }, secretKey)
+        return await jwt.sign(
+            { userName: userName, userID: generatedID },
+            secretKey
+        )
+    }
+
+    static tokenVerifyCallbackIO = (socket, next) => {
+        if (socket.handshake.auth && socket.handshake.auth.token) {
+            jwt.verify(
+                socket.handshake.auth.token,
+                secretKey,
+                (err, decoded) => {
+                    if (err) {
+                        return next(new Error('Authentication error'))
+                    }
+                    socket.decoded = decoded
+                    next()
+                }
+            )
+        } else {
+            next(new Error('Authentication error'))
+        }
     }
 }
 
